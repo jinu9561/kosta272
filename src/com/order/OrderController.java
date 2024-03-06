6fbd356ea6d652f802c210334a387a1bbb6fd6bc
DESKTOP-BSOJMUG\jinwoojung <jinu9561@gmail.com>
2024-03-06 09:13:24 +0900

    no message

--- src/com/order/OrderController.java
+++ src/com/order/OrderController.java
@@ -3,19 +3,18 @@ package com.order;
 public class OrderController {
 
 	public void aasd() {
+		System.out.println("ㅇㅁㄴㅇㄹㄴㅁㄹ");
+		System.out.print("ㄻㄴㅇㅁㄴ");
 		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println("?????????");
 		System.out.println();
 		System.out.println();
 		System.out.println();
 	}
 
     public void rufhfd(){
-        System.out.println("dasdsa");
-        System.out.println("dasdsa");
-        System.out.println("dasdsa");
+        System.out.println("dㄹㄴㅇsa");
+        System.out.println("dasㅎㅎㅁㄹsa");
 
     }
 
